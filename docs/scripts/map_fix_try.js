e4e8cbc2095d3a4fc2209b8ea451bbe12a135649
Dao Ming <daoming@mit.edu>
2020-03-30 09:03:47 +0800

    Leaflet only

--- docs/scripts/map_fix_try.js
+++ docs/scripts/map_fix_try.js
@@ -20,64 +20,6 @@
     minZoom: 9
     }).addTo(map); 
 
-  //Add circle markers to map
-  //tool-tip w/ station name
-  $.get( "data/station_info.csv", function(CSVdata) {
-     // CSVdata is populated with the file contents
-      var station_info = $.csv.toObjects(CSVdata);
-      console.log(station_info)
-
-      var markers = L.markerClusterGroup();
-
-
-      for (var i = 0; i < station_info.length; i++) {
-
-        var MarkerOptions = {
-          radius: 4,
-          fillColor: "#0163FF",
-          color: "#0163FF",
-          weight: 2,
-          opacity: 1,
-          fillOpacity: 0.4,
-        }
-      
-        marker = new L
-        .circleMarker([station_info[i].lat,station_info[i].lon], {
-          radius: 5, //use value
-          color: "#800000", //use value
-        });
-
-        marker.bindTooltip(station_info[i].station_name);
-
-        marker.on('mouseover', function(e) {
-          this.openPopup();
-        })
-
-        marker.on('mouseout', function(e) {
-          this.closePopup();
-        })
-
-        //Show usage stats?
-        //marker.on('click', function(e) {
-        //  //this.
-        //})
-
-        markers.addLayer(marker);
-        marker.addTo(map);
-      }
-      map.addLayer(markers);
-
-    });
-
-    /*
-
-  var circle = L.circle([42.35, -71.08], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(map);
-
   // Add a svg layer to the map
   L.svg().addTo(map); 
 
