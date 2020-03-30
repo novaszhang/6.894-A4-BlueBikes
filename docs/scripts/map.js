1b53108fc4545a3c159f4fc4575947e2fd265799
Dao Ming <daoming@mit.edu>
2020-03-30 09:21:31 +0800

    Updated index.html, map.js, station_info.json

--- docs/scripts/map.js
+++ docs/scripts/map.js
@@ -1,7 +1,9 @@
 /* Map */
 
 // initialize the map
-  var map = L.map('map').setView([42.35, -71.08], 13);
+  var map = new L
+    .map('map')
+    .setView([42.35, -71.08], 13); // center position + zoom
 
   // load a tile layer
   L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
@@ -18,12 +20,50 @@
     minZoom: 9
     }).addTo(map); 
 
-  // load GeoJSON from an external file
-  $.getJSON("data/map.geojson",function(data){
-    // add GeoJSON layer to the map once the file is loaded
-    L.geoJson(data).addTo(map);
-  });
+  //Add circle markers to map
+  //tool-tip w/ station name
+  $.get( "data/station_info.csv", function(CSVdata) {
+     // CSVdata is populated with the file contents
+      var station_info = $.csv.toObjects(CSVdata);
+      console.log(station_info)
 
-d3.csv("data/start-station.csv", function(data) {
-  console.log(data);
-});
+      var markers = L.markerClusterGroup();
+
+      for (var i = 0; i < station_info.length; i++) {
+
+        var MarkerOptions = {
+          radius: 4,
+          fillColor: "#0163FF",
+          color: "#0163FF",
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0.4,
+        }
+      
+        marker = new L
+        .circleMarker([station_info[i].lat,station_info[i].lon], {
+          radius: 5, //use value
+          color: "#800000", //use value
+        });
+
+        marker.bindTooltip(station_info[i].station_name);
+
+        marker.on('mouseover', function(e) {
+          this.openPopup();
+        })
+
+        marker.on('mouseout', function(e) {
+          this.closePopup();
+        })
+
+        //Show usage stats?
+        //marker.on('click', function(e) {
+        //  //this.
+        //})
+
+        markers.addLayer(marker);
+        marker.addTo(map);
+      }
+      map.addLayer(markers);
+
+    });
