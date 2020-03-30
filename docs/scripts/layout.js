e3f6ddcb4f3a5ef0abb315b26b8f1a3a6d113fe5
Dao Ming <daoming@mit.edu>
2020-03-30 20:05:19 +0800

    fix slider v2

--- docs/scripts/layout.js
+++ docs/scripts/layout.js
@@ -1,7 +1,8 @@
 var parentDiv = document.getElementById('slider-step');
 
-var data = [];
 
+
+var data = [];
 for (var i = 0; i <= 23; i++) {
    data.push(i);
 }
@@ -15,16 +16,27 @@ var sliderStep = d3
   .sliderBottom()
   .min(d3.min(dataTime))
   .max(d3.max(dataTime))
-  .width(parentDiv.clientWidth-50)
-  .tickFormat(d3.timeFormat("%H"))
+  .width(parentDiv.clientWidth - 50)
+  .tickFormat(d => customtimeFormat(d))
   .tickValues(dataTime)
   .ticks(24)
-  .step(60 * 60)
+  .step(1000 * 60 * 60)
   .default(new Date('Jan 1, 2019 00:00:00'))
   .on('onchange', val => {
     d3.select('p#value-time').text(d3.timeFormat("%H")(val));
     //sliderHandler(val);
   });
+
+function customtimeFormat(date) {
+  var formatHour = d3.timeFormat("%I")
+      getHour = d3.timeFormat("%H")
+  var data = getHour(date)
+    return        data == 0 ?  "12 am"
+                : data == 12 ? "12 noon"
+                : data == 13 ? "1 pm"
+                : data == 23 ? "11 pm"
+                : data;
+  }
 
 var gStep = d3
   .select('div#slider-step')
@@ -88,3 +100,43 @@ function clickFlow(){
 function sliderHandler(value){
   triggerHandler(true,value); 
 }
+
+
+/*
+var customTimeFormat = timeFormat([
+  ["1:00 am", function () { return true; }],
+  ["2:00", function (d) { return 1 <= d.getHours() && d.getHours() < 2; }],
+  ["3:00", function (d) { return 2 <= d.getHours() && d.getHours() < 3; }],
+
+  ["4:00", function (d) { return 3 <= d.getHours() && d.getHours() < 4; }],
+  ["5:00", function (d) { return 4 <= d.getHours() && d.getHours() < 5; }],
+
+  ["6:00", function (d) { return 5 <= d.getHours() && d.getHours() < 6; }],
+  ["7:00", function (d) { return 6 <= d.getHours() && d.getHours() < 7; }],
+
+  ["8:00", function (d) { return 7 <= d.getHours() && d.getHours() < 8; }],
+  ["9:00", function (d) { return 8 <= d.getHours() && d.getHours() < 9; }],
+
+  ["10:00", function (d) { return 9 <= d.getHours() && d.getHours() < 10; }],
+  ["11:00", function (d) { return 10 <= d.getHours() && d.getHours() < 11; }],
+
+  ["12 noon", function (d) { return 11 <= d.getHours() && d.getHours() < 12; }],
+  ["1:00 pm", function (d) { return 12 <= d.getHours() && d.getHours() < 13; }],
+
+  ["2:00", function (d) { return 13 <= d.getHours() && d.getHours() < 14; }],
+  ["3:00", function (d) { return 14 <= d.getHours() && d.getHours() < 15; }],
+
+  ["4:00", function (d) { return 15 <= d.getHours() && d.getHours() < 16; }],
+  ["5:00", function (d) { return 16 <= d.getHours() && d.getHours() < 17; }],
+
+  ["6:00", function (d) { return 17 <= d.getHours() && d.getHours() < 18; }],
+  ["7:00", function (d) { return 18 <= d.getHours() && d.getHours() < 19; }],
+
+  ["8:00", function (d) { return 19 <= d.getHours() && d.getHours() < 20; }],
+  ["9:00", function (d) { return 20 <= d.getHours() && d.getHours() < 21; }],
+
+  ["10:00", function (d) { return 21 <= d.getHours() && d.getHours() < 22; }],
+  ["11:00 pm", function (d) { return 22 <= d.getHours() && d.getHours() <== 23; }],
+
+]); */
+
